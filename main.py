f3731c8608982ca03b262f22d3b4816dc32374b8
Ignacio Paz <ignaciopaz72@gmail.com>
2021-02-02 11:37:53 -0400

    Arreglos con rutas

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import camelot
 import pandas as pd
 
 
-tables = camelot.read_pdf("../data/horarios.pdf")
+tables = camelot.read_pdf("./data/horarios.pdf")
 # tables.export("prueba.csv", f = "csv", compress=False)
 
 table_horarios = tables[0].df
 table_asignatura = tables[1].df
 
-table_horarios.to_json("./out/horarios.json")
-table_asignatura.to_json("../out/asignaturas.json")
-
+# table_horarios.to_json("./out/horarios.json")
+# table_asignatura.to_json("../out/asignaturas.json")
 
 
 # print(table_horarios.size)
