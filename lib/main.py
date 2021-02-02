22be1fef9438da6f331233030b1e7bc707355971
Ignacio Paz <ignaciopaz72@gmail.com>
2021-02-02 00:59:51 -0400

    Extraccion de datos del PDF y conv a Dataframe

---
+++ lib/main.py
@@ -0,0 +1,18 @@
+import camelot
+import pandas as pd
+
+
+tables = camelot.read_pdf("../data/horarios.pdf")
+# tables.export("prueba.csv", f = "csv", compress=False)
+
+table_horarios = tables[0].df
+table_asignatura = tables[1].df
+
+table_horarios.to_json("./out/horarios.json")
+table_asignatura.to_json("../out/asignaturas.json")
+
+
+
+# print(table_horarios.size)
+# print(table_horarios)
+print(table_asignatura)
